6401c485b8c59c33a319bcd781e1467826e16783
outtaken <liubko81@gmail.com>
2018-03-18 22:37:27 +0300

    First commit

---
+++ Assets/scripts/pleyercontlorel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pleyercontlorel : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+
+		
+	}
+     void FixedUpdate() 
+    {
+        float moveHorisontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input .GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorisontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
